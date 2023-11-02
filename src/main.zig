1b7e8816f3723e41088b562b2deabde8069684b5
Kevin Blaesing <kevin.blaesing@gmail.com>
2023-11-02 11:36:38 -0400

    fix: actual working implementation of making and parsing todos

--- src/main.zig
+++ src/main.zig
@@ -4,18 +4,32 @@ const Todo = lib_todo.Todo;
 const print = std.debug.print;
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
 
-    // const allocator = gpa.allocator();
-    // const td = try Todo.from_line("Hello! +World", allocator);
+    var input_buffer: [1024]u8 = undefined;
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
 
-    // print("{s}, {any}", .{ td.description, td.tags });
-    const text = "Hello! +World &G";
+    const allocator = arena.allocator();
 
-    var it = std.mem.splitScalar(u8, text, '+');
+    while (true) {
+        try stdout.print("Enter the description: ", .{});
 
-    while (it.next()) |item| {
-        print("{s}\n", .{item});
+        _ = try stdin.readUntilDelimiter(&input_buffer, '\n');
+
+        // TODO: (hahaha), this is a bit rough to read.
+        var td = Todo.fromLine(&input_buffer, allocator) catch |err| {
+            switch (err) {
+                lib_todo.Errors.FoundMultipleGroups => {
+                    try stdout.print("Can't have multiple groups!\n", .{});
+                    return;
+                },
+                else => continue,
+            }
+        };
+        defer td.deinit();
+
+        try stdout.print("{s}\n", .{td.description.items});
     }
 }
--- src/todo.zig
+++ src/todo.zig
@@ -1,64 +1,114 @@
 const std = @import("std");
 const t = std.testing;
 const expect = std.testing.expect;
+const ArrayList = std.ArrayList;
 
-const Errors = error{
+pub const Errors = error{
     EmptyLineError,
+    FoundMultipleGroups,
 };
 
+/// A `Todo` holds its `description`, `tags` and `group`.
+/// A `Todo` is written like so:
+///
+/// Make bread +Party
+/// OR
+/// Write that whitepaper +Work &A
+/// Write that whitepaper &A +Work
+///
+/// Its format is `description <tags> <group>`.
 pub const Todo = struct {
     const Self = @This();
 
-    description: []const u8,
-    tags: [][]const u8,
-    group: u8,
+    description: ArrayList(u8),
+    tags: ArrayList([]const u8),
+    group: u8, // Single character
+    raw_length: usize,
 
     pub fn fromLine(line: []const u8, allocator: std.mem.Allocator) !Self {
         if (line.len == 0) {
             return Errors.EmptyLineError;
         }
 
-        var group: u8 = '-'; // Start with empty group.
+        var description = ArrayList(u8).init(allocator);
+        var group: u8 = '-';
+        var tags = ArrayList([]const u8).init(allocator);
 
-        var description_words = std.ArrayList(u8).init(allocator);
-        defer description_words.deinit();
+        const first_tag_index = std.mem.indexOfScalar(u8, line, '+') orelse line.len;
+        const group_index = std.mem.indexOfScalar(u8, line, '&') orelse line.len;
 
-        var tags = std.ArrayList([]const u8).init(allocator);
-        defer tags.deinit();
+        if (group_index != line.len) {
+            const maybe_second_group = std.mem.lastIndexOfScalar(u8, line[group_index + 1 ..], '&');
 
-        var tokens = std.mem.splitScalar(u8, line, ' ');
+            if (maybe_second_group != null) {
+                return Errors.FoundMultipleGroups;
+            }
+            group = line[group_index + 1];
+        }
+
+        if (first_tag_index != line.len) {
+            var tag_tokens = std.mem.tokenizeScalar(u8, line[first_tag_index..group_index], ' ');
+
+            while (tag_tokens.next()) |tag| {
+                try tags.append(tag[1..]);
+            }
+        }
+
+        var words = std.mem.tokenizeScalar(u8, line[0..first_tag_index], ' ');
+
+        while (words.next()) |word| {
+            try description.appendSlice(word);
+
+            // TODO: this probably isn't the best way to check this.
+            if (words.peek() != null) {
+                try description.append(' ');
+            }
+        }
 
         return .{
             .description = description,
             .group = group,
-            .tags = try tags.toOwnedSlice(),
+            .tags = tags,
+            .raw_length = line.len,
         };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.description.deinit();
+        self.tags.deinit();
+    }
+
+    /// Return a nicely formatted string of this object (how it was written).
+    pub fn format_self(self: Self) []const u8 {
+        _ = self;
     }
 };
 
+test "Can't make an empty Todo" {
+    try t.expectError(Errors.EmptyLineError, Todo.fromLine("", t.allocator));
+}
+
+test "Can't have multiple groups in a Todo" {
+    try t.expectError(Errors.FoundMultipleGroups, Todo.fromLine("&A &B", t.allocator));
+}
+
 test "Make a Todo" {
-    const td = try Todo.fromLine("Make Bread", t.allocator);
-    try expect(std.mem.eql(u8, td.description, "Make Bread"));
+    var td = try Todo.fromLine("Hello World! +Newbie &A", t.allocator);
+    defer td.deinit();
 
-    // Default group.
-    try expect(td.group == '-');
+    try t.expectEqualSlices(u8, "Hello World!", td.description.items);
+    try t.expect(td.group == 'A');
+    const expected_tags = &[_][]const u8{"Newbie"};
 
-    // Should have no tags.
-    try t.expectEqualSlices([]const u8, &[_][]const u8{}, td.tags);
+    for (expected_tags, td.tags.items) |expected, actual| {
+        try t.expectEqualSlices(u8, expected, actual);
+    }
 }
 
-test "Can't init empty line" {
-    try std.testing.expectError(Errors.EmptyLineError, Todo.fromLine("", t.allocator));
-}
+test "Check formatting" {
+    const line = "Hello World! +Newbie &A";
+    var td = try Todo.fromLine(line, t.allocator);
+    defer td.deinit();
 
-test "Make a todo with a tag" {
-    const td = try Todo.fromLine("Make Bread +Party", t.allocator);
-
-    try t.expectEqualSlices([]const u8, &[_][]const u8{"Party"}, td.tags);
-}
-
-test "Make a todo with a group" {
-    const td = try Todo.fromLine("Make Bread &P", t.allocator);
-
-    try expect(td.group == 'P');
+    try t.expectEqualSlices(u8, line, td.format_self());
 }
